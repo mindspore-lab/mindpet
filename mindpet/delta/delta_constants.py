f604e545acd8d3c78424565070c910195a8129a1
koukairui <koukairui@huawei.com>
2024-01-05 09:54:56 +0800

    support bfloat16

--- mindpet/delta/delta_constants.py
+++ mindpet/delta/delta_constants.py
@@ -5,4 +5,4 @@
 from mindspore import dtype as mstype
 
 # 算法精度与类型控制
-VALID_TENSOR_DATATYPE = [mstype.float16, mstype.float32]
+VALID_TENSOR_DATATYPE = [mstype.float16, mstype.float32, mstype.bfloat16]
